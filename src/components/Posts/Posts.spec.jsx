2ea007a18960fc15b80a15109edd963c9264e989
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2023-10-19 23:27:01 -0300

    testing Posts coverage 100%

--- src/components/Posts/Posts.spec.jsx
+++ src/components/Posts/Posts.spec.jsx
@@ -1,9 +1,43 @@
-import {Post} from '.';
+import {Posts} from '.';
 import { render, screen } from "@testing-library/react";
 
+const props = {
+  posts:[
+    {
+      id: 1,
+      title: 'title 1',
+      body: 'body 1',
+      cover: 'img/img1.png'
+    },
+    {
+      id: 2,
+      title: 'title 2',
+      body: 'body 2',
+      cover: 'img/img2.png'
+    },
+    {
+      id: 3,
+      title: 'title 3',
+      body: 'body 3',
+      cover: 'img/img3.png'
+    },
+  ]
+}
 
 describe('<Post />', () => {
-    test('dummy test', () => {
-        expect(1).toBe(1);
-      });
+  it('should render Post correctly', () => {
+    render(<Posts {...props}/>);
+    expect(screen.getAllByRole('heading', {name: /title/i})).toHaveLength(3);
+  });
+
+  it('should match snapshot', () => {
+    const {container} = render(<Posts {...props}/>);
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should not render posts', () => {
+    render(<Posts />);
+    expect(screen.queryByRole('heading', {name: /title/i})).not.toBeInTheDocument();
+  });
 });
